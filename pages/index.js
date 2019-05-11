2b645e2c5e49fd9af4ecb510921d20354f805948
VanishMax <maxkors2014@gmail.com>
2019-05-11 22:26:25 +0300

    3 types of grid are supported

--- pages/index.js
+++ pages/index.js
@@ -35,20 +35,93 @@ export default class Home extends React.Component {
         <div className='hero'>
           <h1 className='title'>Redux Cat-counter</h1>
 
-          <div className="quote rounded shadow-lg my-8 overflow-hidden p-4 text-lg text-justify">
-            <p dangerouslySetInnerHTML={{__html: this.props.quote}}/>
-            <p className="text-right italic pt-4">{this.props.author}</p>
-          </div>
-
-          <div className="card rounded overflow-hidden shadow-lg my-8">
-            <div className="w-full catPhoto">
-              {this.state.url && <img className="w-full" src={this.state.url} alt="Catass"/>}
+          <div className="default-grid">
+            <div className="flex md:hidden lg:hidden">
+              <div className="w-full my-8 mx-2">
+                <div className="rounded shadow-lg text-lg p-4">
+                  <p className="text-justify" dangerouslySetInnerHTML={{__html: this.props.quote}}/>
+                  <p className="text-right italic pt-4">{this.props.author}</p>
+                </div>
+              </div>
             </div>
 
-            <div className="px-6 py-6">
-              <div className="font-bold text-xl mb-2 text-center">Modern Web App</div>
+            <div className="flex md:hidden lg:hidden">
+              <div className="w-full my-8 mx-2">
+                <div className="rounded shadow-lg text-lg p-4">
+                  <p>Kekes</p>
+                </div>
+              </div>
+            </div>
+
+            <div className="flex md:hidden lg:hidden">
+              <div className="w-full rounded shadow-lg my-8 mx-2">
+                <div className="w-full">
+                  {this.state.url && <img className="w-full rounded" src={this.state.url} alt="Catass"/>}
+                </div>
+
+                <div className="px-6 py-6">
+                  <div className="font-bold text-xl mb-2 text-center">Modern Web App</div>
+                </div>
+              </div>
+            </div>
+        </div>
+
+          <div className="middle-grid">
+            <div className="hidden md:flex lg:hidden">
+              <div className="md:w-1/2 my-8 mx-2">
+                <div className="rounded shadow-lg text-lg p-4">
+                  <p className="text-justify" dangerouslySetInnerHTML={{__html: this.props.quote}}/>
+                  <p className="text-right italic pt-4">{this.props.author}</p>
+                </div>
+              </div>
+
+              <div className="md:w-1/2 my-8 mx-2">
+                <div className="rounded shadow-lg text-lg p-4">
+                  <p>Kekes</p>
+                </div>
+              </div>
+            </div>
+            <div className="hidden md:flex lg:hidden">
+              <div className="md:w-full rounded shadow-lg my-8 mx-2">
+                <div className="w-full">
+                  {this.state.url && <img className="w-full rounded" src={this.state.url} alt="Catass"/>}
+                </div>
+
+                <div className="px-6 py-6">
+                  <div className="font-bold text-xl mb-2 text-center">Modern Web App</div>
+                </div>
+              </div>
             </div>
           </div>
+
+          <div className="large-grid">
+            <div className="hidden lg:flex">
+
+              <div className="w-1/4 my-8 mx-2">
+                <div className="rounded shadow-lg text-lg p-4">
+                  <p className="text-justify" dangerouslySetInnerHTML={{__html: this.props.quote}}/>
+                  <p className="text-right italic pt-4">{this.props.author}</p>
+                </div>
+              </div>
+
+              <div className="w-1/2 rounded shadow-lg my-8 mx-2">
+                <div className="w-full">
+                  {this.state.url && <img className="w-full rounded" src={this.state.url} alt="Catass"/>}
+                </div>
+
+                <div className="px-6 py-6">
+                  <div className="font-bold text-xl mb-2 text-center">Modern Web App</div>
+                </div>
+              </div>
+
+              <div className="w-1/4 my-8 mx-2">
+                <div className="rounded shadow-lg text-lg p-4">
+                  <p>Kekes</p>
+                </div>
+              </div>
+            </div>
+          </div>
+
         </div>
       </div>
     )
