22f1ebfb42184ac9360345b4981723dcc19ebd1d
VanishMax <maxkors2014@gmail.com>
2019-05-12 14:20:52 +0300

    Better counter styles

--- components/counter.js
+++ components/counter.js
@@ -2,14 +2,14 @@ import React from 'react'
 
 export default () => (
   <React.Fragment>
-    <h3 className="text-xl font-semibold text-center">
+    <h3 className="text-xl font-semibold text-center my-2">
       Counter itself
     </h3>
+    <h3 className="text-xl text-center">0</h3>
     <div className="text-center">
       <button className="btn-gray-outlined">
         Increase
       </button>
-      <span className="text-xl">0</span>
       <button className="btn-gray-outlined">
         Decrease
       </button>
