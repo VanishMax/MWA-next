55df9731e1f8eee2a9eddd19f2ca0cabac24c59f
VanishMax <maxkors2014@gmail.com>
2019-05-12 12:29:37 +0300

    Fixed navigation flex problems
    Now it is fully adaptive

--- components/nav.js
+++ components/nav.js
@@ -2,21 +2,21 @@ import Link from 'next/link'
 
 export default function Nav () {
   return (
-    <nav className="flex items-center justify-between flex-wrap bg-gray-700 p-3">
-      <div className="flex items-center flex-shrink-0 text-white mr-6">
+    <nav class="flex items-center justify-between flex-wrap bg-gray-700 p-3">
+      <div class="flex items-center flex-shrink-0 text-white mr-6">
         <Link prefetch href='/'>
-          <a className='text-white text-xl font-bold py-1 px-4 rounded no-underline'>Modern Web App</a>
+          <a className='text-white text-xl font-bold py-1 px-4 rounded no-underline hover:text-gray-200'>Modern Web App</a>
         </Link>
       </div>
-      <div className='w-full block flex-grow lg:flex lg:items-center lg:w-auto'>
-        <div className='lg:flex-grow'>
+      <div class="flex w-auto items-center flex-grow">
+        <div class="text-sm flex-grow">
           <Link prefetch href='/about'>
-            <a className='text-white py-2 px-4 no-underline'>About</a>
+            <a className='text-white py-2 px-4 no-underline text-lg hover:text-gray-200'>About</a>
           </Link>
         </div>
         <div>
           <Link href='https://github.com/vanishmax/modern-web-app'>
-            <a className='btn bg-white text-black font-bold py-2 px-4 rounded no-underline' target='_blank'>GitHub</a>
+            <a className='btn bg-white text-black font-bold py-1 px-4 rounded no-underline hover:bg-gray-200' target='_blank'>GitHub</a>
           </Link>
         </div>
       </div>
