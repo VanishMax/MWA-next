7e67a89053b63c99fbb14fd7c930bb838a16b48a
VanishMax <maxkors2014@gmail.com>
2019-05-12 12:39:31 +0300

    Bugs fixed, no redundant styles

--- components/nav.js
+++ components/nav.js
@@ -2,14 +2,14 @@ import Link from 'next/link'
 
 export default function Nav () {
   return (
-    <nav class="flex items-center justify-between flex-wrap bg-gray-700 p-3">
-      <div class="flex items-center flex-shrink-0 text-white mr-6">
+    <nav className="flex items-center justify-between flex-wrap bg-gray-700 p-3">
+      <div className="flex items-center flex-shrink-0 text-white mr-6">
         <Link prefetch href='/'>
           <a className='text-white text-xl font-bold py-1 px-4 rounded no-underline hover:text-gray-200'>Modern Web App</a>
         </Link>
       </div>
-      <div class="flex w-auto items-center flex-grow">
-        <div class="text-sm flex-grow">
+      <div className="flex w-auto items-center flex-grow">
+        <div className="text-sm flex-grow">
           <Link prefetch href='/about'>
             <a className='text-white py-2 px-4 no-underline text-lg hover:text-gray-200'>About</a>
           </Link>
