0477d22d9343025e07165de0407425ff07ed796f
VanishMax <maxkors2014@gmail.com>
2019-05-12 13:05:07 +0300

    Refactoring

--- pages/index.js
+++ pages/index.js
@@ -32,7 +32,8 @@ export default class Home extends React.Component {
         <div className='hero'>
           <h1 className='title font-comforta'>Redux Cat-counter</h1>
 
-          <div className="default-grid">
+          {/* Small and middle viewports */}
+          <div>
             <div className="flex md:hidden lg:hidden">
               <div className="w-full my-8 mx-2">
                 <div className="rounded shadow-lg text-lg p-4">
@@ -50,36 +51,23 @@ export default class Home extends React.Component {
               </div>
             </div>
 
-            <div className="flex md:hidden lg:hidden">
+            <div className="hidden md:flex lg:hidden">
+              <div className="md:w-1/2 my-8 mx-2">
+                <div className="rounded shadow-lg text-lg p-4">
+                  <p className="text-justify" dangerouslySetInnerHTML={{__html: this.props.quote}}/>
+                  <p className="text-right italic pt-4">{this.props.author}</p>
+                </div>
+              </div>
+
+              <div className="md:w-1/2 my-8 mx-2">
+                <div className="rounded shadow-lg text-lg p-4">
+                  <p>Kekes</p>
+                </div>
+              </div>
+            </div>
+
+            <div className="flex md:flex lg:hidden">
               <div className="w-full rounded shadow-lg my-8 mx-2">
-                <div className="w-full">
-                  {this.state.url && <img className="w-full rounded" src={this.state.url} alt="Catass"/>}
-                </div>
-
-                <div className="px-6 py-6">
-                  <div className="font-bold text-xl mb-2 text-center">Modern Web App</div>
-                </div>
-              </div>
-            </div>
-        </div>
-
-          <div className="middle-grid">
-            <div className="hidden md:flex lg:hidden">
-              <div className="md:w-1/2 my-8 mx-2">
-                <div className="rounded shadow-lg text-lg p-4">
-                  <p className="text-justify" dangerouslySetInnerHTML={{__html: this.props.quote}}/>
-                  <p className="text-right italic pt-4">{this.props.author}</p>
-                </div>
-              </div>
-
-              <div className="md:w-1/2 my-8 mx-2">
-                <div className="rounded shadow-lg text-lg p-4">
-                  <p>Kekes</p>
-                </div>
-              </div>
-            </div>
-            <div className="hidden md:flex lg:hidden">
-              <div className="md:w-full rounded shadow-lg my-8 mx-2">
                 <div className="w-full">
                   {this.state.url && <img className="w-full rounded" src={this.state.url} alt="Catass"/>}
                 </div>
@@ -91,7 +79,8 @@ export default class Home extends React.Component {
             </div>
           </div>
 
-          <div className="large-grid">
+          {/* Large viewport */}
+          <div>
             <div className="hidden lg:flex">
 
               <div className="w-1/4 my-8 mx-2">
